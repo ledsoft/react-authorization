566a2583cde37e6ad8dc773b19b61c3526df5c56
Martin Ledvinka <martin.ledvinka@hotmail.com>
2021-11-09 21:18:19 +0100

    [Fix] Fix missing export of IfAuthorized from module.

--- src/react-authorization.js
+++ src/react-authorization.js
@@ -2,5 +2,6 @@ import IfAllGranted from "./IfAllGranted";
 import IfAnyGranted from "./IfAnyGranted";
 import IfGranted from "./IfGranted";
 import IfNoneGranted from "./IfNoneGranted"
+import IfAuthorized from "./IfAuthorized";
 
-export {IfAllGranted, IfAnyGranted, IfGranted, IfNoneGranted};
+export {IfAllGranted, IfAnyGranted, IfGranted, IfNoneGranted, IfAuthorized};
